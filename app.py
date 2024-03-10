84194fd1c94f0599d190d71719f0bfe291ef4c49
hieupham1103 <phamdinhtrunghieu1103@gmail.com>
2024-03-10 14:21:27 +0700

    test server add log

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, jsonify, request 
-from flask_apscheduler import APScheduler
-import atexit
 import random
 
 
@@ -9,18 +7,9 @@ fish_data = {
     "cachep": [50]
 }
 
-class Config:
-    SCHEDULER_API_ENABLED = True
-    
 app = Flask(__name__)
-app.config.from_object(Config())
-scheduler = APScheduler()
-scheduler.init_app(app)
-scheduler.start()
 
-@scheduler.task('interval', id='do_job_1', seconds=0.2)
 def update_data():
-    print("update_data")
     global fish_data
     for fish in fish_data:
         fish_data[fish].append(fish_data[fish][-1] + random.uniform(-1,1))
@@ -29,6 +18,7 @@ def update_data():
 
 @app.route("/get_price")
 def get_price():
+    update_data()
     fish = request.args.get('fish') 
     if fish in fish_data:
         return jsonify({'price': fish_data[fish][-1]}) 
