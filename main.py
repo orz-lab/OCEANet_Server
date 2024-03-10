1189422e1321892af8d387eb106ffc8894b61c11
Phạm Đình Trung Hiếu <62142713+hieupham1103@users.noreply.github.com>
2024-03-10 03:46:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request 
+from threading import Timer
+import random
+
+
+MAX_LOG = 1000
+fish_data = {
+    "cachep": [50]
+}
+
+def update_data(interval):
+    Timer(interval, update_data, [interval]).start()
+    global fish_data
+    for fish in fish_data:
+        fish_data[fish].append(fish_data[fish][-1] + random.uniform(-1,1))
+        if  len(fish_data[fish]) >= MAX_LOG:
+            fish_data[fish].pop(0)
+
+update_data(1)
+
+app = Flask(__name__)
+
+@app.route("/get_price")
+def get_price():
+    fish = request.args.get('fish') 
+    if fish in fish_data:
+        return jsonify({'price': fish_data[fish][-1]}) 
+    return jsonify({}) 
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+
+
